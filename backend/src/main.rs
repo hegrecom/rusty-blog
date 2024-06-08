d991a0055bdd0b735d30339a5bcc61b28c0f6509
Teekey <hegrecomm@gmail.com>
2024-06-08 16:26:33 +0900

    refactor: Extract database pool and routes functions from  main function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -21,14 +21,28 @@ mod schema;
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+
+    let app = admin_routes()
+        .merge(public_routes())
+        .with_state(database_pool())
+        .layer(middleware::from_fn(method_not_allowed_handler))
+        .fallback(not_found_handler);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn database_pool() -> deadpool_diesel::postgres::Pool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
     let manager =
         deadpool_diesel::postgres::Manager::new(database_url, deadpool_diesel::Runtime::Tokio1);
-    let pool = deadpool_diesel::postgres::Pool::builder(manager)
+    deadpool_diesel::postgres::Pool::builder(manager)
         .build()
-        .expect("Could not create database pool");
+        .expect("Could not create database pool")
+}
 
-    let admin_routes = Router::new()
+fn admin_routes() -> Router<deadpool_diesel::postgres::Pool> {
+    Router::new()
         .route(
             "/admins/posts",
             get(admin_post_controller::index).post(admin_post_controller::create),
@@ -43,18 +57,12 @@ async fn main() {
             "/admins/posts/:post_id/publish",
             post(admin_post_controller::publish),
         )
-        .route_layer(from_extractor::<RequireAuthorization>());
-    let public_routes = Router::new()
+        .route_layer(from_extractor::<RequireAuthorization>())
+}
+
+fn public_routes() -> Router<deadpool_diesel::postgres::Pool> {
+    Router::new()
         .route("/admins/login", post(admin_controller::login))
         .route("/posts", get(post_controller::index))
-        .route("/posts/:post_id", get(post_controller::show));
-
-    let app = admin_routes
-        .merge(public_routes)
-        .with_state(pool)
-        .layer(middleware::from_fn(method_not_allowed_handler))
-        .fallback(not_found_handler);
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+        .route("/posts/:post_id", get(post_controller::show))
 }
